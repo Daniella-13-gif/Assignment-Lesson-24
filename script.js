0edb00bdce9428a001a651ff327bc3fc50b9bad8
Daniella-13-gif <ngozihomestore@gmail.com>
2025-01-09 22:12:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function buttonfunction() {
+    document.getElementById("demo").innerHTML="a+b"
+    document.write("Your satisfaction our priority")
+    document.write('<p class="style1"></p>');
+    
+}
